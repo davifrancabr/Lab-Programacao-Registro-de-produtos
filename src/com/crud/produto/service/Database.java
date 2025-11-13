4d5e983798467ed1889f388fcea60f6fc477cb7c
davifranca <davicavalcantefranca@gmail.com>
2025-11-13 11:14:12 -0300

    fix: changed controller logics

--- src/com/crud/produto/service/Database.java
+++ src/com/crud/produto/service/Database.java
@@ -1,23 +1,29 @@
 package com.crud.produto.service;
 
-import java.util.ArrayList;
 
-public class Database {
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database  {
     private int id;
     private String nome;
     private double preco;
     private int quantidade;
 
-    ArrayList<Database> database = new ArrayList<>();
-
-    private void adicionarDatabase(int id, String nome, double preco, int quantidade){
-        Database database = new Database();
-        database.id = id;
-        database.nome = nome;
-        database.preco = preco;
-        database.quantidade = quantidade;
-        this.database.add(database);
+    List<Database> databases = new ArrayList<Database>();
+    public Database(int id, String nome, double preco, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+    private void addDatabase(int id, String nome, double preco, int quantidade){
+        databases.add(new Database(this.id,this.nome, this.preco,this.quantidade));
     }
 
 
+    public void adicionarDatabase(int id, String nome, double preco, int quantidade) {
+        this.addDatabase(id, nome, preco, quantidade);
+    }
 }
--- src/com/teste/produto/ProdutoTeste.java
+++ src/com/teste/produto/ProdutoTeste.java
@@ -4,8 +4,8 @@ import com.crud.produto.controller.ProductController;
 
 public class ProdutoTeste {
     public static void main(String[] args) {
-        ProductController p;
-        p = new ProductController();
+
+        ProductController p = new ProductController();
         p.adicionarProduto(1,"Davi", 2.5,2);
     }
 }
